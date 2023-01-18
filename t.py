10f0f6b54d3898ef115f6ec35f7491986bdc0b25
dashl <lcraton@gmail.com>
2023-01-17 17:24:56 -0800

    tests now working

--- t.py
+++ t.py
@@ -7,16 +7,17 @@ conn = psycopg.connect("dbname='test' user='dashl' host='localhost'")
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
-cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
+# cur.execute("CREATE TABLE test (id serial PRIMARY KEY, num integer, data varchar);")
 
 # Pass data to fill a query placeholders and let Psycopg perform
 # the correct conversion (no more SQL injections!)
-cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (100, "abc'def"))
+cur.execute("INSERT INTO test (num, data) VALUES (%s, %s)", (171, "nothing to see here"))
 
 # Query the database and obtain data as Python objects
 cur.execute("SELECT * FROM test;")
 cur.fetchone()
-# (1, 100, "abc'def")
+cur.fetchone()
+# (2, 171, "nothing to see")
 
 # Make the changes to the database persistent
 conn.commit()
